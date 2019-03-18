5d2a313ea4e40f7fec79e440737e6d2b28ed37a5
Jason Huang <350578794@qq.com>
2019-03-18 13:50:53 +0800

    commit

---
+++ framework-admin/src/main/java/com/framework/AdminApplication.java
@@ -0,0 +1,23 @@
+package com.framework;
+
+import com.framework.modules.job.config.ScheduleConfig;
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.quartz.QuartzAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+@MapperScan(basePackages = { "com.framework.modules.*.dao" })
+public class AdminApplication extends SpringBootServletInitializer {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(AdminApplication.class);
+	}
+}
